24c83282b6b5f27a7dde01d5f299c8504069f731
Ryo Osawa <ryojiosawa@gmail.com>
2014-10-13 15:47:45 -0700

    Add server dir and schedules.js.  Also, move lib dir under server

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var twilio = require('./lib/twilio');
-var bart = require('./lib/bart');
+var twilio = require('./server/lib/twilio');
+var bart = require('./server/lib/bart');
 
 bart.fetchDepartureInfo('powl', 'n', 'RICH', function(estimates) {
   twilio.sendText('415-724-9329', 'RICH', estimates);
