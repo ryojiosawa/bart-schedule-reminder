df31e08904c0bdd11c3573d9d3275ab1ecc4e6ac
Ryo Osawa <ryojiosawa@gmail.com>
2014-10-13 16:20:11 -0700

    Add db-config to connect to MongoDB

--- server/db-config.js
+++ server/db-config.js
@@ -1,3 +1,13 @@
-/**
- * Created by rosawa on 10/13/14.
- */
+var mongoose = require('mongoose');
+
+mongoURI = process.env.MONGO_URI || 'mongodb://localhost/bart-schedule-reminder';
+mongoose.connect(mongoURI);
+
+// Run in seperate terminal window using 'mongod'
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongodb connection open');
+});
+
+module.exports = db;
