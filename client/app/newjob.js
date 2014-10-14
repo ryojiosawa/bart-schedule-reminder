6f7e700fdff8a5830c3c7ddd4803f95dcf2e96c3
Ryo Osawa <ryojiosawa@gmail.com>
2014-10-14 01:55:56 -0700

    Add a new form view to create a schedule job

--- client/app/newjob.js
+++ client/app/newjob.js
@@ -2,6 +2,7 @@ angular.module('bart-schedule-reminder.newjob', [])
 
   .controller('NewJobController', function ($scope, $location, JobService) {
     $scope.job = {};
+
     $scope.addJob = function() {
       JobService.addJob($scope.job)
         .then(function() {
@@ -11,4 +12,19 @@ angular.module('bart-schedule-reminder.newjob', [])
           console.error(error);
         });
     };
+
+    $scope.stations = [
+      {abbr: '12th', name: '12th St. Oakland City Center'},
+      {abbr: '16th', name: '16th St. Mission (SF)'},
+      {abbr: '19th', name: '19th St. Oakland'},
+      {abbr: '24th', name: '24th St. Mission (SF)'},
+      {abbr: 'ashb', name: 'Ashby (Berkeley)'},
+      {abbr: 'balb', name: 'Balboa Park (SF)'},
+      {abbr: 'bayf', name: 'Bay Fair (San Leandro)'},
+      {abbr: 'cast', name: 'Castro Valley'},
+      {abbr: 'civc', name: 'Civic Center (SF)'},
+      {abbr: 'cols', name: 'Coliseum/Oakland Airport'},
+      {abbr: 'embr', name: 'Embarcadero (SF)'},
+      {abbr: 'powl', name: 'Powell St. (SF)'}
+    ];
   });
