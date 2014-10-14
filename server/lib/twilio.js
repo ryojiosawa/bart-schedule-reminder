867e5113c60a5766ce6a10126c90e418684f916e
Ryo Osawa <ryojiosawa@gmail.com>
2014-10-13 18:58:17 -0700

    Externalize some settings

--- server/lib/twilio.js
+++ server/lib/twilio.js
@@ -1,8 +1,7 @@
-// Twilio Credentials
 var _ = require('underscore');
-var accountSid = 'ACca9e3c2a49c5ee9d18dca2636da601aa';
-var authToken = '200751a731f2ba8e2b193d2175ef2585';
-var from = "+14152756980";
+var accountSid = process.env.TWILIO_ACCOUNT_ID;
+var authToken = process.env.AUTH_TOKEN;
+var from = process.env.FROM;
 
 //require the Twilio module and create a REST client
 var client = require('twilio')(accountSid, authToken);
