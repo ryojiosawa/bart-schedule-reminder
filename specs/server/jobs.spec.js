6ffecab496849b017010f533948716f87915b359
Ryo Osawa <ryojiosawa@gmail.com>
2014-12-08 16:59:14 -0800

    Add more tests for job-controller

--- specs/server/jobs.spec.js
+++ specs/server/jobs.spec.js
@@ -6,6 +6,13 @@ var Job = require('../../server/jobs/job-model');
 
 describe('Job Test Suite', function() {
 
+  beforeEach(function(done) {
+    Job.remove()
+      .exec(function() {
+        done();
+      });
+  });
+
   describe('POST: /jobs', function() {
     it('should return an error message upon invalid request', function(done) {
       request(app)
@@ -39,33 +46,29 @@ describe('Job Test Suite', function() {
     });
   });
 
-  xdescribe('GET: /jobs', function() {
-    it('should return a list of jobs', function(done) {
-      db.ReportedCase.bulkCreate([
-        {
-          disease_id: disease_id,
-          latitude: 12.678,
-          longitude: 23.456,
-          date: Date.now(),
-          description: "Ebola is bad"
-        },
-        {
-          disease_id: disease_id,
-          latitude: 12.345,
-          longitude: 23.456,
-          date: Date.now(),
-          description: "Ebola is really bad"
-        }
-      ])
-        .success(function(cases) {
-          request(app)
-            .get('/api/cases')
-            .expect(200)
-            .end(function(err, res) {
-              expect(res.body.length).to.be.equal(2);
-              expect(res.body[0].latitude).to.equal(12.678);
+  describe('GET: /jobs', function() {
+    beforeEach(function(done) {
+      Job.remove()
+        .exec(function() {
+          Job.create([
+            { phone: "123-456-789", station: "powl", destination: "rich", direction: "n" },
+            { phone: "123-456-789", station: "powl", destination: "rich", direction: "n" }
+          ])
+            .then(function() {
               done();
-            });
+            })
+        });
+    });
+
+    it('should return a list of jobs', function(done) {
+      request(app)
+        .get('/jobs')
+        .expect(200)
+        .end(function(err, res) {
+          expect(res.body.length).to.be.equal(2);
+          expect(res.body[0].phone).to.equal("123-456-789");
+          expect(res.body[0].station).to.equal("powl");
+          done();
         });
     });
   });
@@ -100,15 +103,20 @@ describe('Job Test Suite', function() {
     });
   });
 
-  xdescribe('DELETE: /jobs/job_id', function() {
-    it('should return "Method not allowed" error', function(done) {
-      request(app)
-        .delete('/api/cases/1')
-        .expect(404)
-        .end(function(req, res) {
-          expect(res.body.message).to.be.equal('Method not allowed');
-          done();
-        });
-    });
+  it.only('DELETE: /jobs/job_id should delete a specified job', function(done) {
+    Job.create({ phone: "123-456-789", station: "powl", destination: "rich", direction: "n" })
+      .then(function(res) {
+
+        request(app)
+          .delete('/jobs/' + res._id)
+          .expect(200)
+          .end(function(req, res) {
+            expect(res.body.message).to.equal('Successfully deleted');
+            Job.findOne({_id: res._id})
+              .exec(function(res) {
+                done();
+              });
+          });
+      });
   });
 });
