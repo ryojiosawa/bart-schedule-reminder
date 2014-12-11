d5e349fe71c613893d2fdf208fc4a0001c182265
Ryo Osawa <ryojiosawa@gmail.com>
2014-12-10 22:56:07 -0800

    Refactor fetchDepartureInfo method to use Promise

--- server/lib/bart.js
+++ server/lib/bart.js
@@ -1,9 +1,9 @@
 var qs = require('querystring');
-var parseString = require('xml2js').parseString;
-var request = require('request');
-var _ = require('underscore');
+var Promise = require('bluebird');
+var request = Promise.promisifyAll(require('request'));
+var parseString = Promise.promisify(require('xml2js').parseString);
 
-var bartApiEndpoint = 'http://api.bart.gov/api/etd.aspx?cmd=etd&key=MW9S-E7SL-26DU-VV8V&';
+var BART_API_ENDPOINT = 'http://api.bart.gov/api/etd.aspx?cmd=etd&key=MW9S-E7SL-26DU-VV8V&';
 
 var fetchDepartureInfo = function(orig, dir, dest, cb) {
   var querystring = qs.stringify({
@@ -11,31 +11,13 @@ var fetchDepartureInfo = function(orig, dir, dest, cb) {
     dir: dir
   });
 
-  var url = bartApiEndpoint + querystring;
+  var url = BART_API_ENDPOINT + querystring;
   console.log('Sending a request to ', url);
 
-  request(url, function (err, resp, body) {
-    if (err) throw err;
-
-    var xml = body;
-
-    parseString(xml, function (err, json) {
-      var estimates = _.filter(json.root.station[0].etd, function (estimate) {
-        if (estimate.abbreviation[0] === dest.toUpperCase()) {
-          return estimate.estimate;
-        }
-      });
-
-      if (estimates && estimates[0] && estimates[0].estimate) {
-        estimates = estimates[0].estimate;
-        var minutes = _.map(estimates, function (estimate) {
-          return estimate.minutes[0];
-        });
-
-        cb(minutes);
-      }
+  return request.getAsync(url)
+    .then(function(resp) {
+      return parseString(resp[0].body);
     });
-  });
 };
 
 exports.fetchDepartureInfo = fetchDepartureInfo;
--- server/worker.js
+++ server/worker.js
@@ -3,12 +3,29 @@ var async = require('async');
 var bart = require('./lib/bart');
 var twilio = require('./lib/twilio');
 var JobCtrl = require('./jobs/job-controller');
+var _ = require('underscore');
 
 JobCtrl.fetchJobs(function(jobs) {
   async.each(jobs, function(job, done) {
-    bart.fetchDepartureInfo(job.station, job.direction, job.destination, function (estimates) {
-      console.log('estimates', estimates);
-      twilio.sendText(job, estimates, done);
+    bart.fetchDepartureInfo(job.station, job.direction, job.destination)
+      .then(function(json) {
+
+        var dest = job.destination.toUpperCase();
+        var estimates = _.filter(json.root.station[0].etd, function (estimate) {
+          if (estimate.abbreviation[0] === dest) {
+            return estimate.estimate;
+          }
+        });
+
+        if (estimates && estimates[0] && estimates[0].estimate) {
+          estimates = estimates[0].estimate;
+          var minutes = _.map(estimates, function (estimate) {
+            return estimate.minutes[0];
+          });
+
+          console.log('estimates', estimates);
+          //twilio.sendText(job, estimates, done);
+        }
     });
   }, function(err) {
     if (err) console.error(err);
