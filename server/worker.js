f77b75606443386283bc876f5ff10bdb5227c320
Ryo Osawa <ryojiosawa@gmail.com>
2014-12-11 10:54:59 -0800

    Refactor worker, twilio and bart modules to use Promises

--- server/worker.js
+++ server/worker.js
@@ -1,35 +1,41 @@
-var db = require('./db-config');
+var db = require('./db-config'); // this is needed in order for worker to access MongoDB
 var async = require('async');
 var bart = require('./lib/bart');
 var twilio = require('./lib/twilio');
-var JobCtrl = require('./jobs/job-controller');
+var fetchJobs = require('./jobs/job-controller').fetchJobs;
 var _ = require('underscore');
 
-JobCtrl.fetchJobs(function(jobs) {
-  async.each(jobs, function(job, done) {
-    bart.fetchDepartureInfo(job.station, job.direction, job.destination)
-      .then(function(json) {
+fetchJobs()
+  .then(function(jobs) {
+    async.each(jobs, function(job, done) {
+      bart.fetchDepartureInfo(job.station, job.direction, job.destination)
+        .then(function(json) {
 
-        var dest = job.destination.toUpperCase();
-        var estimates = _.filter(json.root.station[0].etd, function (estimate) {
-          if (estimate.abbreviation[0] === dest) {
-            return estimate.estimate;
-          }
-        });
-
-        if (estimates && estimates[0] && estimates[0].estimate) {
-          estimates = estimates[0].estimate;
-          var minutes = _.map(estimates, function (estimate) {
-            return estimate.minutes[0];
+          var dest = job.destination.toUpperCase();
+          var estimates = _.filter(json.root.station[0].etd, function (estimate) {
+            if (estimate.abbreviation[0] === dest) {
+              return estimate.estimate;
+            }
           });
 
-          console.log('estimates', estimates);
-          //twilio.sendText(job, estimates, done);
-        }
+          if (estimates && estimates[0] && estimates[0].estimate) {
+            estimates = estimates[0].estimate;
+            var minutes = _.map(estimates, function (estimate) {
+              return estimate.minutes[0] === 'Leaving' ? 'now': estimate.minutes[0];
+            });
+
+            console.log('estimates', minutes);
+            twilio.sendText(job, minutes)
+             .then(function(message) {
+                console.log(message.sid);
+                done();
+              }, function(err) {
+                done(err);
+              });
+          }
+      });
     });
   }, function(err) {
-    if (err) console.error(err);
-
+    console.log(err);
     process.exit(0);
   });
-});
